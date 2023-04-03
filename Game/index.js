03a30c3de3a92de5447556404a831ee0bb6b51c3
aditya-goel <adityagoel0320@gmail.com>
2023-04-04 02:06:50 +0530

    04/04/23

---
+++ Game/index.js
@@ -0,0 +1,59 @@
+let arr = [
+    {
+        'name': 'CSS',
+        'img': 'http://thapatechnical.online/logos/css.png',
+    },
+    {
+        'name': 'HTML',
+        'img': 'http://thapatechnical.online/logos/html5.png',
+    },
+    {
+        'name': 'jQuery',
+        'img': 'http://thapatechnical.online/logos/jquery.png',
+    },
+    {
+        'name': 'JS',
+        'img': 'http://thapatechnical.online/logos/js.png',
+    },
+    {
+        'name': 'Node',
+        'img': 'http://thapatechnical.online/logos/nodejs.png',
+    },
+    {
+        'name': 'Python',
+        'img': 'http://thapatechnical.online/logos/python.png',
+    } , 
+    {
+        'name': 'CSS',
+        'img': 'http://thapatechnical.online/logos/css.png',
+    },
+    {
+        'name': 'HTML',
+        'img': 'http://thapatechnical.online/logos/html5.png',
+    },
+    {
+        'name': 'jQuery',
+        'img': 'http://thapatechnical.online/logos/jquery.png',
+    },
+    {
+        'name': 'JS',
+        'img': 'http://thapatechnical.online/logos/js.png',
+    },
+    {
+        'name': 'Node',
+        'img': 'http://thapatechnical.online/logos/nodejs.png',
+    },
+    {
+        'name': 'Python',
+        'img': 'http://thapatechnical.online/logos/python.png',
+    }
+];
+let box = document.getElementById("box")
+for (let i = 0; i <= arr.length - 1; i++) {
+    let child_box = document.createElement("div")
+    child_box.classList.add("childbox")
+    child_box.style.backgroundImage = `url(${arr[i].img})`
+    // box.appendChild(child_box)
+    box.appendChild(child_box)
+}
+
